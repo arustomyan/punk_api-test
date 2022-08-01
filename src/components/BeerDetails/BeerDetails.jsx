b6e77db5668e3dee5940a2545b4e30ab03cc3ac3
varustomyan <v.arustomyan@icloud.com>
2022-08-01 03:30:41 +0300

    future: added button back

--- src/components/BeerDetails/BeerDetails.jsx
+++ src/components/BeerDetails/BeerDetails.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { getBeer } from "../../api/PunkApi";
 import { useFetching } from "../../hooks/useFetching";
 import Loader from "../Loader/Loader";
@@ -8,6 +8,7 @@ import styles from "./BeerDetails.module.css";
 function BeerDetails() {
   const [beer, setBeer] = useState({});
   const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
 
   const params = useParams();
 
@@ -24,41 +25,48 @@ function BeerDetails() {
     fetchBeer(params.id);
   }, []);
 
-  return (
-    <div className={styles.component}>
-      {isLoading ? (
-        <Loader />
-      ) : (
-        <>
-          <div className={styles.cover}>
-            <div className={styles.imageWrapper}>
-              <img
-                className={styles.image}
-                src={beer.image_url}
-                alt={`Cover of ${beer.name}`}
-              />
-            </div>
-          </div>
-          <div className={styles.details}>
-            <p className={styles.name}>{beer.name}</p>
-            <p className={styles.tagline}>{beer.tagline}</p>
-            <div className={styles.specifications}>
-              <p className={styles.abv}>ABV: {beer.abv}</p>
-              <p className={styles.abv}>IBU: {beer.ibu}</p>
-              <p className={styles.ebc}>EBC: {beer.ebc}</p>
-            </div>
+  if (isLoading) {
+    return <Loader />;
+  }
 
-            <div className={styles.food_pairing}>
-              <h3>Food Pairings:</h3>
-              <ul>
-                {beer.food_pairing &&
-                  beer.food_pairing.map((food) => <li key={food}>{food}</li>)}
-              </ul>
-            </div>
-            <p className={styles.description}>{beer.description}</p>
+  return (
+    <div className={styles.page}>
+      <button
+        type="button"
+        onClick={() => navigate(-1)}
+        className={styles.button}
+      >
+        Back
+      </button>
+      <div className={styles.component}>
+        <div className={styles.cover}>
+          <div className={styles.imageWrapper}>
+            <img
+              className={styles.image}
+              src={beer.image_url}
+              alt={`Cover of ${beer.name}`}
+            />
           </div>
-        </>
-      )}
+        </div>
+        <div className={styles.details}>
+          <p className={styles.name}>{beer.name}</p>
+          <p className={styles.tagline}>{beer.tagline}</p>
+          <div className={styles.specifications}>
+            <p className={styles.abv}>ABV: {beer.abv}</p>
+            <p className={styles.abv}>IBU: {beer.ibu}</p>
+            <p className={styles.ebc}>EBC: {beer.ebc}</p>
+          </div>
+
+          <div className={styles.food_pairing}>
+            <h3>Food Pairings:</h3>
+            <ul>
+              {beer.food_pairing &&
+                beer.food_pairing.map((food) => <li key={food}>{food}</li>)}
+            </ul>
+          </div>
+          <p className={styles.description}>{beer.description}</p>
+        </div>
+      </div>
     </div>
   );
 }
